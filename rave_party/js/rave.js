0f4df810c4a8b7e070566b47d26c7652ae5aaf4f
Gary Larson <gary.larson316@gmail.com>
2021-01-31 12:40:28 -0800

    unfinished javascript

---
+++ rave_party/js/rave.js
@@ -0,0 +1,26 @@
+//javascript for rave party
+var count = 1;
+function playsound() {
+    
+}
+
+function createRoom() {
+    //insert description
+    //change title
+    //change header
+    //change options
+        //insert into text and ref of anchor (id = opt1 opt2 ... 3)
+}
+function delay(mi) {
+    
+}
+
+class Room {
+    //links is a dictionary of linkName linkRef pairs
+    constructor(title, descrip, links ) {
+        this.title = title;
+        this.descrip = descrip;
+        this.links = links;
+    }
+    
+}
