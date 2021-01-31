6d1103fafda37725e82868e415eda7488e56340c
Gary Larson <gary.larson316@gmail.com>
2021-01-31 13:30:41 -0800

    experimenting with javascript

--- rave_party/js/rave.js
+++ rave_party/js/rave.js
@@ -1,9 +1,4 @@
 //javascript for rave party
-var count = 1;
-function playsound() {
-    
-}
-
 function createRoom() {
     //insert description
     //change title
@@ -11,8 +6,11 @@ function createRoom() {
     //change options
         //insert into text and ref of anchor (id = opt1 opt2 ... 3)
 }
-function delay(mi) {
-    
+function startGame() {
+    audio.play();
+}
+function delay(milliseconds) {
+    //in javascript use timeout function
 }
 
 class Room {
