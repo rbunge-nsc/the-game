918d971799a9bf149d6fb3f154e681d96a0e6074
Gary Larson <gary.larson316@gmail.com>
2021-02-04 18:13:33 -0800

    adding js

--- room_template/js/main.js
+++ room_template/js/main.js
@@ -1,24 +1,28 @@
-//javascript for rave party
-function createRoom() {
-    //insert description
-    //change title
-    //change header
-    //change options
-        //insert into text and ref of anchor (id = opt1 opt2 ... 3)
+
+
+//functions to generate rooms
+function room1() {
+    document.write("room1");
 }
-function startGame() {
-    audio.play();
+function room2() {
+    document.write("room2");
 }
-function delay(milliseconds) {
-    //in javascript use timeout function
+function room3() {
+    document.write("room3");
+}
+function room4() {
+    document.write("room4");
 }
 
-class Room {
-    //links is a dictionary of linkName linkRef pairs
-    constructor(title, descrip, links ) {
-        this.title = title;
-        this.descrip = descrip;
-        this.links = links;
-    }
-    
+//helper functions 
+function createLI() {
+    var node = document.createElement("LI");                 // Create a <li> node
+    var textnode = document.createTextNode("Water");         // Create a text node
+    node.appendChild(textnode);                              // Append the text to <li>
+    document.getElementById("myList").appendChild(node);     // Append <li> to <ul> with id="myList"
+}
+function createButton(descr) {
+    var btn = document.createElement("BUTTON");   // Create a <button> element
+    btn.innerHTML = descr;                   // Insert text
+    document.onclick="room1()"               // change onclick attribute
 }
